044b51aa49811b294de16fdf30e5229cd3092272
Matheus Meigre <142261154+matheusmeigre@users.noreply.github.com>
2024-03-22 17:13:02 -0300

    project clock finalized

---
+++ js/script.js
@@ -0,0 +1,34 @@
+let digitalElement = document.querySelector(".digital");
+let secondElement = document.querySelector(".p_s");
+let minuteElement = document.querySelector(".p_m");
+let hourElement = document.querySelector(".p_h");
+
+function updateClock() {
+    //PARTE DIGITAL
+
+    let now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+
+    digitalElement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(
+        second
+    )}`;
+
+    //PARTE ANALÓGICA
+
+    let secondDeg = (360 / 60) * second - 90;
+    let minuteDeg = (360 / 60) * minute - 90;
+    let hourDeg = (360 / 12) * hour - 90;
+
+    secondElement.style.transform = `rotate(${secondDeg}deg)`;
+    minuteElement.style.transform = `rotate(${minuteDeg}deg)`;
+    hourElement.style.transform = `rotate(${hourDeg}deg)`;
+}
+
+function fixZero(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
